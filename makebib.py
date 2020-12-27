3f5ed1421bb552877de6eeb8741d8d1f0267e9a8
MakerTobey <MakerTobey@users.noreply.github.com>
2020-12-27 18:35:37 -0300

    make sense of the code and clean

--- makebib.py
+++ makebib.py
@@ -3,24 +3,24 @@
 import yaml
 import os
 
-#get .bib for files with arXiv identifier
-with open(r'_data/publist.yml') as file:
-    pubs = yaml.load(file, Loader=yaml.FullLoader)
+# #get .bib for files with arXiv identifier
+# with open(r'_data/publist.yml') as file:
+#     pubs = yaml.load(file, Loader=yaml.FullLoader)
 
-    for pub in pubs:
-        for k,v in pub.items():
-            if (k=='url'):
-                url=v
-            if (k=='arxiv'):
-                print('working on ArXiv file ' + url)
-                os.system("./scripts/arxiv2bib.py " + str(v) + " > papers/" + url +".txt")
-                infile = open("papers/" + url +".txt",'r')
-                newopen = open("papers/tmp.bib",'w')
-                for line in infile:
-                    if 'File' not in line and 'Eprint' not in line and 'PrimaryClass' not in line:
-                        newopen.write(line)
-                newopen.close()
-                os.system("mv papers/tmp.bib papers/" + url + ".txt")
+#     for pub in pubs:
+#         for k,v in pub.items():
+#             if (k=='url'):
+#                 url=v
+#             if (k=='arxiv'):
+#                 print('working on ArXiv file ' + url)
+#                 os.system("./scripts/arxiv2bib.py " + str(v) + " > papers/" + url +".txt")
+#                 infile = open("papers/" + url +".txt",'r')
+#                 newopen = open("papers/tmp.bib",'w')
+#                 for line in infile:
+#                     if 'File' not in line and 'Eprint' not in line and 'PrimaryClass' not in line:
+#                         newopen.write(line)
+#                 newopen.close()
+#                 os.system("mv papers/tmp.bib papers/" + url + ".txt")
 
 #get .bib for files with DOI 
 with open(r'_data/publist.yml') as file:
